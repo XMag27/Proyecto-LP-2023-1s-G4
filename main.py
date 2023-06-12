31408247bc6b8c79151d7feee7b131d87c57dce4
GhostWarrior25 <luidbrav@espol.edu.ec>
2023-06-11 19:39:27 -0500

    [ADD] new Algorithm [UPD] tuple Tokens, with news tokens [UPD] dictionary reserved, with news reserved words [UPD] news regular expressions symbols [FIX] error in t_OR

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import ply.lex as lex
-#Analizador léxico para Rust
 
-#Diccionario de palabras reservadas
+# Analizador léxico para Rust
+
+# Diccionario de palabras reservadas
 reserved = {
-#Xavier Magallanes
-  'println': 'PRINT',
-  'let': 'LET',
-  'mut': 'MUT',
+    # Xavier Magallanes
+    'println': 'PRINT',
+    'let': 'LET',
+    'mut': 'MUT',
     'fn': 'FN',
     'main': 'MAIN',
     'if': 'IF',
@@ -23,16 +24,27 @@ reserved = {
     'enum': 'ENUM',
     'impl': 'IMPL',
     'use': 'USE',
+    'vec': 'VEC',
+    'static': 'STATIC',
+    'type': 'TYPE',
+    'ref': 'REF',
+    'match': 'MATCH',
+    'as': 'AS',
+    'mod': 'MOD',
+    'trait': 'TRAIT',
+    'pub': 'PUB',
 
 }
 
-#Sequencia de tokens, puede ser lista o tupla
+# Sequencia de tokens, puede ser lista o tupla
 tokens = ('NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
           'LLLAVE', 'RLLAVE', 'VARIABLE', 'CADENA', 'PUNTOYCOMA',
-          'EXCLAMACION', 'IGUAL', 'COMMA', 'L_BRACKET', 'R_BRACKET','DOUBLE_POINT','POINT','PERCENTAGE','AND','OR') + tuple(reserved.values())
+          'EXCLAMACION', 'IGUAL', 'COMMA', 'L_BRACKET', 'R_BRACKET', 'DOUBLE_POINT', 'POINT', 'PERCENTAGE', 'AND', 'OR',
+          'L_DIAMOND', 'R_DIAMOND', 'EQUAL_EQUAL', 'NOT_EQUAL', 'LESS_EQUAL', 'GREATER_EQUAL', 'ARROW',
+          'AMPERSAND') + tuple(reserved.values())
 
-#Exp Regulares para tokens de símbolos
-#Xavier Magallanes
+# Exp Regulares para tokens de símbolos
+# Xavier Magallanes
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_COMMA = r'\,'
@@ -50,47 +62,77 @@ t_L_BRACKET = r'\['
 t_R_BRACKET = r'\]'
 t_DOUBLE_POINT = r'\:'
 t_POINT = r'\.'
-t_PERCENTAGE= r'\%'
+t_PERCENTAGE = r'\%'
 t_AND = r'\&&'
-t_OR =r'\||'
+t_OR = r'\|\|'
+# Donoso Bravo Luis Alejandro
+t_L_DIAMOND = r'\<'
+t_R_DIAMOND = r'\>'
+t_EQUAL_EQUAL = r'\=\='
+t_NOT_EQUAL = r'\!\='
+t_LESS_EQUAL = r'\<\='
+t_GREATER_EQUAL = r'\>\='
+t_AMPERSAND = r'\&'
+t_ARROW = r'\->'
 
-#Para contabilizar nro de líneas
+
+# Para contabilizar nro de líneas
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 
 def t_VARIABLE(t):
-  r'(_{1}|[a-zA-Z]{1})[a-zA-Z0-9_]*'
-  t.type = reserved.get(t.value, 'VARIABLE')
-  return t
+    r'(_{1}|[a-zA-Z]{1})[a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'VARIABLE')
+    return t
 
 
 def t_CADENA(t):
-  r'\"{1}.*"{1}'
-  t.type = reserved.get(t.value, 'CADENA')
-  return t
+    r'\"{1}.*"{1}'
+    t.type = reserved.get(t.value, 'CADENA')
+    return t
 
 
 def t_COMMENT(t):
-  r'\/{2}.*'
-  pass
-  # No return value. Token discarded
+    r'\/{2}.*'
+    pass
+    # No return value. Token discarded
+
+#Donoso Bravo Luis Alejandro
+def t_Option(t):
+    r'Option'
+    t.type = reserved.get(t.value, 'OPTION')
+    return t
+
+def t_Some(t):
+    r'Some'
+    t.type = reserved.get(t.value, 'SOME')
+    return t
+
+def t_None(t):
+    r'None'
+    t.type = reserved.get(t.value, 'NONE')
+    return t
+
+def t_Array(t):
+    r'array'
+    t.type = reserved.get(t.value, 'ARRAY')
+    return t
 
 
 # Ignorar lo que no sea un token en mi LP
 t_ignore = ' \t'
 
 
-#Presentación de errores léxicos
+# Presentación de errores léxicos
 def t_error(t):
-  print("Componente léxico no reconocido '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print("Componente léxico no reconocido '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 
-#Contruir analizador
+# Contruir analizador
 lexer = lex.lex()
-
 
 data = '''
     let mut x = 5;
@@ -123,14 +165,46 @@ let num: i32 = range.gen();
 
 println!("Random: {}", n1);
 }
+
+// Donoso Bravo Luis Alejandro
+fn binary_search<T>: Ord>(array: &[T], target: &T) -> Option<usize> {
+    let mut left = 0;
+    let mut right = array.len() - 1;
+
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        
+        if array[mid] == *target {
+            return Some(mid);
+        } else if array[mid] < *target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    None
+}
+
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let target = 6;
+    
+    if let Some(index) = binary_search(&numbers, &target) {
+        println!("Target {} found at index {}", target, index);
+    } else {
+        println!("Target {} not found", target);
+    }
+}
+
     '''
 
-#Datos de entrada
+# Datos de entrada
 lexer.input(data)
 
 # Tokenizador
 while True:
-  tok = lexer.token()
-  if not tok:
-    break  #Rompe
-  print(tok)
+    tok = lexer.token()
+    if not tok:
+        break  # Rompe
+    print(tok)
